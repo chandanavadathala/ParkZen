237004db7cfd2195b774b30de5399bd6c878f247
Shekhar-Shelke <shekharshelke45@gmail.com>
2026-02-22 12:51:22 +0530

    push the files

---
+++ backend/src/main/java/com/parkzen/ParkZen_stress_free_parking_experience/entity/Admin.java
@@ -0,0 +1,26 @@
+package com.parkzen.ParkZen_stress_free_parking_experience.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String adminName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String mobile;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ backend/src/test/java/com/parkzen/ParkZen_stress_free_parking_experience/ParkZenStressFreeParkingExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkzen.ParkZen_stress_free_parking_experience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkZenStressFreeParkingExperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
