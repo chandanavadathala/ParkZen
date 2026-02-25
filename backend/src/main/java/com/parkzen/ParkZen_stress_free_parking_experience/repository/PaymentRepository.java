c4bb820c4213d223adfba9694a2a843e5be15025
Shekhar-Shelke <shekharshelke45@gmail.com>
2026-02-25 11:32:50 +0530

    update application.properties file.

--- backend/src/main/java/com/parkzen/ParkZen_stress_free_parking_experience/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.parkzen.ParkZen_stress_free_parking_experience.repository;
-
-import com.parkzen.ParkZen_stress_free_parking_experience.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
