16b3727c9178721430542366b71756d9e5a77d36
Shekhar-Shelke <shekharshelke45@gmail.com>
2026-03-25 08:17:08 +0530

    Added full project

---
+++ backend/src/main/java/com/parkzen/ParkZen_stress_free_parking_experience/ParkzenStressFreeParkingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parkzen.Parkzen_stress_free_parking_experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  ParkzenStressFreeParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkzenStressFreeParkingSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/parkzen/ParkZen_stress_free_parking_experience/ParkzenStressFreeParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkzen.Parkzen_stress_free_parking_experience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkzenStressFreeParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
